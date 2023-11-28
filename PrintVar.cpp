a22164ae1a93bb0237d28416dbf9f47365d061a8
ItxKabeerAhmad <152272069+ItxKabeerAhmad@users.noreply.github.com>
2023-11-28 17:01:40 +0500

    Add files via upload

---
+++ PrintVar.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int age = 17;
+    string name = "Kabeer";
+    float MatricMarksPercentage = 96.6;
+    char grade = 'C';
+    cout<<"I AM "<<name<<" AND I AM "<<age<<"y OLD. MY MATRIC MARKS PERCENTAGE IS "<<MatricMarksPercentage<<". I GOT "<<grade<<" GRADE IN INTERMEDIATE";
+}
