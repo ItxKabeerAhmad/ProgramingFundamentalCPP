ad0e90c3010b6c2b102df1da4f5fbc06def32921
ItxKabeerAhmad <152272069+ItxKabeerAhmad@users.noreply.github.com>
2023-11-28 16:41:49 +0500

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include<iostream> //header file to use input output function
+using namespace std; /* is a declaration in C++ that specifies that you want to use the std (standard) namespace throughout your code without explicitly qualifying each element from that namespace.
+*/
+int main()//program execution starts from here
+{//every function start from curly barces and also ends with them
+    
+    cout << "Hello, World ^_^" << endl;//outputs the format string
+    return 0; //returns 0 and exit the function
+}
